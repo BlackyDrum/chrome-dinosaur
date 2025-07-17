b030a0439cf0376ecf8e9075cda0fa35a5357c27
BlackyDrum <ganiaytan1010@web.de>
2025-07-17 15:00:21 +0200

    Add highscore

--- include/UI.h
+++ include/UI.h
@@ -3,6 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #include "Globals.h"
 
@@ -22,13 +23,17 @@ public:
     uint32_t GetScore() const { return m_Score; }
 private:
     sf::Font m_Font;
-    sf::Text m_ScoreText;
-    sf::Text m_GameOverText;
+    
+    sf::Text m_HighScoreText;
+    uint32_t m_HighScore = 0;
+    const std::string m_HighScoreFile = "data/highscore.txt";
 
+    sf::Text m_ScoreText;
     uint32_t m_Score = 0;
     float m_AccumulatedScore = 0.f;
     float m_ScoreUpdateInterval = 0.1f;
 
+    sf::Text m_GameOverText;
     sf::Sprite m_RestartButton;
     sf::Texture m_Texture;
     sf::IntRect m_RestartButtonRect;
--- src/UI.cpp
+++ src/UI.cpp
@@ -1,7 +1,7 @@
 #include "UI.h"
 
 UI::UI()
-    : m_ScoreText(sf::Text(m_Font, "00000", 20)), m_GameOverText(sf::Text(m_Font, "G A M E  O V E R", 30)), m_RestartButton(m_Texture)
+    : m_ScoreText(sf::Text(m_Font, "00000", 20)), m_GameOverText(sf::Text(m_Font, "G A M E  O V E R", 30)), m_HighScoreText(sf::Text(m_Font, "HI 00000", 20)), m_RestartButton(m_Texture)
 {
     if (!m_Font.openFromFile("assets/PressStart2P.ttf"))
         std::cerr << "Failed to load font!\n";
@@ -16,6 +16,29 @@ UI::UI()
     m_ScoreText.setPosition(sf::Vector2f(SCREEN_WIDTH - 10.f, 10.f));
     m_ScoreText.setFillColor(gray);
 
+    sf::FloatRect highScoreBounds = m_HighScoreText.getLocalBounds();
+    float spacing = 20.f; // Spacing between score and high score
+    m_HighScoreText.setOrigin(sf::Vector2f(highScoreBounds.position.x + highScoreBounds.size.x, 0.f));
+    m_HighScoreText.setPosition(sf::Vector2f(SCREEN_WIDTH - 10.f - scoreBounds.size.x - spacing, 10.f));
+    m_HighScoreText.setFillColor(gray);
+
+    std::ifstream highScoreFile(m_HighScoreFile);
+    if (highScoreFile.is_open())
+    {
+        highScoreFile >> m_HighScore;
+        highScoreFile.close();
+
+        if (m_HighScore > 99999)
+            m_HighScore = 99999;
+    }
+    else
+    {
+        std::cerr << "Failed to open high score file!\n";
+        m_HighScore = 0;
+    }
+
+    UpdateScoreText();
+
     sf::FloatRect goBounds = m_GameOverText.getLocalBounds();
     m_GameOverText.setOrigin(sf::Vector2f(goBounds.position.x + goBounds.size.x / 2.f, goBounds.position.y + goBounds.size.y / 2.f));
     m_GameOverText.setPosition(sf::Vector2f(SCREEN_WIDTH / 2.f, SCREEN_HEIGHT / 2.f - 40.f));
@@ -29,6 +52,23 @@ UI::UI()
 void UI::SetGameOver(bool isOver)
 {
     m_IsGameOver = isOver;
+
+    if (m_IsGameOver && m_Score > m_HighScore)
+    {
+        m_HighScore = m_Score;
+        std::ofstream highScoreFile(m_HighScoreFile);
+        if (highScoreFile.is_open())
+        {
+            highScoreFile << m_HighScore;
+            highScoreFile.close();
+        }
+        else
+        {
+            std::cerr << "Failed to save high score!\n";
+        }
+
+        UpdateScoreText();
+    }
 }
 
 void UI::UpdateScoreText()
@@ -36,11 +76,16 @@ void UI::UpdateScoreText()
     std::stringstream ss;
     ss << std::setfill('0') << std::setw(5) << m_Score;
     m_ScoreText.setString(ss.str());
+
+    std::stringstream ssHighScore;
+    ssHighScore << std::setfill('0') << std::setw(5) << m_HighScore;
+    m_HighScoreText.setString("HI " + ssHighScore.str());
 }
 
 void UI::Draw(sf::RenderWindow& window)
 {
     window.draw(m_ScoreText);
+    window.draw(m_HighScoreText);
 
     if (m_IsGameOver)
     {
@@ -60,8 +105,9 @@ void UI::Update(sf::Time deltaTime)
     {
         m_Score++;
         m_AccumulatedScore = 0.f;
-        UpdateScoreText();
     }
+
+    UpdateScoreText();
 }
 
 void UI::Reset()
@@ -70,4 +116,6 @@ void UI::Reset()
     m_AccumulatedScore = 0;
     m_IsGameOver = false;
     m_ScoreText.setString("00000");
+
+    UpdateScoreText();
 }
